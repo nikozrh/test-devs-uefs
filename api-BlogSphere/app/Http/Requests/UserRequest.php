f4fc94145127afc97c07b1a24ff84ecccd6bbf1f
sandoelio <sandoelio@hotmail.com>
2025-03-11 15:47:18 -0300

    Aprimora validação de senha no UserRequest e adiciona mensagens de erro personalizadas para campos obrigatórios.

--- api-BlogSphere/app/Http/Requests/UserRequest.php
+++ api-BlogSphere/app/Http/Requests/UserRequest.php
@@ -19,7 +19,27 @@ class UserRequest extends FormRequest
 
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users,email,' . $this->route('user'),
-            'password' => $this->isMethod('post') ? 'required|string|min:6' : 'nullable|string|min:6'
+            'password' => $this->isMethod('post') ? 'required|string|min:8' : 'nullable|string|min:8'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O campo "Nome" é obrigatório.',
+            'name.string' => 'O campo "Nome" deve ser uma string.',
+            'name.max' => 'O campo "Nome" não pode ter mais que 255 caracteres.',
+            'email.required' => 'O campo "E-mail" é obrigatório.',
+            'email.email' => 'O campo "E-mail" deve ser um e-mail válido.',
+            'email.unique' => 'O e-mail informado já está em uso.',
+            'password.required' => 'O campo "Senha" é obrigatório.',
+            'password.string' => 'O campo "Senha" deve ser uma string.',
+            'password.min' => 'O campo "Senha" deve ter no mínimo 8 caracteres.'
         ];
     }
 }
+
+
+
+
+ 
